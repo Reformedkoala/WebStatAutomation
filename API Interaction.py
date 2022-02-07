e4a2fb319c4cfd6602c8dfec2668d95bc8abe7be
Reformedkoala <aightb22@gmail.com>
2022-02-07 14:54:45 -0700

    Finally made an API Request and have data in correct format
    yay

--- API Interaction.py
+++ API Interaction.py
@@ -1,20 +1,20 @@
 import matomo_api as ma
 import requests
-'''
+import json
+
 URL = 'https://mineslibrary.matomo.cloud'
 TOKEN = '8510379384a55b02e47fe2c52665e13b'
+
 api = ma.MatomoApi(URL, TOKEN)
-'''
+
 pars = ma.format.csv | ma.language.en | ma.translateColumnNames() \
-       | ma.idSite.all | ma.date.lastMonth | ma.period.month \
+       | ma.idSite.one_or_more(1) | ma.date.lastMonth | ma.period.month \
        | ma.showColumns(ma.col.nb_visits, ma.col.nb_uniq_visitors, ma.col.nb_pageviews) \
        | ma.filter_limit(25)
 
-my_headers = {'Authorization' : 'Bearer  {8510379384a55b02e47fe2c52665e13b}'}
-response = requests.get('https://mineslibrary.matomo.cloud', headers=my_headers, params=pars)
-print(response.json())
 
-"""
-with open('test_output_data.csv') as test_file:
-       test_file.write(response)
-"""
+response = api.Actions().getExitPageUrls(pars)
+with open('test_output_data.csv', 'w') as test_output:
+    for i in response.text.split(','):
+        test_output.write(i)
+        test_output.write(',')
